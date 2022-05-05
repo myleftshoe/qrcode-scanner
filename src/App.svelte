72f94d1f2d31b011bb07b7ddcbdecad91258c463
myleftshoe <paulesammut@gmail.com>
2022-05-05 19:42:52 +1000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+    import { Html5Qrcode } from 'html5-qrcode'
+    import { onMount } from 'svelte'
+
+    let reader
+    let scanning = false
+
+    let html5Qrcode
+
+    onMount(init)
+
+    function init() {
+        // fix for iOS, otherwiser reader ref is not required
+        reader.setAttribute('autoplay', '')
+        reader.setAttribute('muted', '')
+        reader.setAttribute('playsinline', '')
+
+        html5Qrcode = new Html5Qrcode('reader')
+    }
+
+    function start() {
+        html5Qrcode.start(
+            { facingMode: 'environment' },
+            {
+                fps: 10,
+                qrbox: { width: 250, height: 250 },
+                disableFlip: true
+            },
+            onScanSuccess,
+            onScanFailure
+        )
+        scanning = true
+    }
+
+    async function stop() {
+        await html5Qrcode.stop()
+        scanning = false
+    }
+
+    function onScanSuccess(decodedText, decodedResult) {
+        alert(`Code matched = ${decodedText}`, decodedResult)
+    }
+
+    function onScanFailure(error) {
+        console.warn(`Code scan error = ${error}`)
+    }
+</script>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    }
+    #reader {
+        width: 100%;
+        min-height: 500px;
+        background-color: black;
+    }
+</style>
+
+<main>
+    <div id="reader" bind:this={reader} />
+    {#if scanning}
+        <button on:click={stop}>stop</button>
+    {:else}
+        <button on:click={start}>start</button>
+    {/if}
+</main>
